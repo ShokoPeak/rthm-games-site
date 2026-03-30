005ebad8c6c2b6d2b0c4cff4fdb323993d162a52
Matthew Holmes <matthewholmes1337@hotmail.co.uk>
2026-03-30 20:15:41 +0100

    Add initial site with Eleventy, blog layout, and first post

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
+  });
+
+  eleventyConfig.addFilter("dateFormat", function (date) {
+    return new Date(date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data",
+    },
+  };
+};
